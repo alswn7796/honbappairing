7b998222d5ab1e9a19a0a7b991a15b64b0d07819
alswn7796 <alswn7796@gmail.com>
2025-11-04 17:09:02 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
+<!-- filename: config.js -->
 // config.js  (HTML 태그 넣지 마세요!)
 window.firebaseConfig = {
     apiKey: "AIzaSyB0TUXQpzZIy0v2gbLOC343Jx_Lv51EQvw",
@@ -7,6 +8,3 @@ window.firebaseConfig = {
     messagingSenderId: "375771626039",
     appId: "1:375771626039:web:03868631de56225cf49db2",
 };
-
-// ✅ 관리자 이메일 목록(원하면 여러 개 추가 가능)
-window.ADMIN_EMAILS = ["qkdtjwns123@kw.ac.kr"];
